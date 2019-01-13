f5beff298f659a36adaea712025895940b09f166
Dylan Pavelko <dpavelko@gmail.com>
2019-01-13 06:37:49 +0000

    removed relationship on pose

--- db/migrate/20170510061457_add_modification_of_to_poses.rb
+++
@@ -1,5 +0,0 @@
-class AddModificationOfToPoses < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :poses, :modification_of, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,6 @@ ActiveRecord::Schema.define(version: 20170512032622) do
     t.integer "difficulty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "modification_of_id"
-    t.index ["modification_of_id"], name: "index_poses_on_modification_of_id"
   end
 
   create_table "routines", force: :cascade do |t|
